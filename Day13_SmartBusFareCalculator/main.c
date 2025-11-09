0199b8fcff823b3022883f61b2b36257122b2b5c
suryakaushikkaligotla <kaushik_kaligotla@srmap.edu.in>
2025-11-09 16:02:47 +0530

    Create main.c

---
+++ Day13_SmartBusFareCalculator/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+int main() {
+    int totalpassengers;
+    int i;
+    char name[50];
+    int age;
+    float distance;
+    int time;
+    float basefare, finalfare;
+    float totalrevenue = 0;
+    float highestfare = 0;
+    float lowestfare = 99999;
+
+    printf("----------SRM Smart Bus Fare Calculator----------\n");
+    printf("Enter number of passengers: ");
+    scanf("%d", &totalpassengers);
+
+    for (i = 1; i <= totalpassengers; i++) {
+        printf("\n------Passenger %d------\n", i);
+
+        printf(" Please Enter name: ");
+        scanf("%s", name);
+
+        printf("Please Enter age: ");
+        scanf("%d", &age);
+
+        printf("Please Enter distance (in km): ");
+        scanf("%f", &distance);
+
+        printf("Please Enter travel time (24-hour format): ");
+        scanf("%d", &time);
+
+        basefare = distance * 10;
+        finalfare = basefare;
+
+        if (age < 12) {
+            finalfare = basefare * 0.5;
+            printf("Passenger: %s - Base Fare: ₹%.2f - Child Discount Applied", name, basefare);
+        } 
+        else if (age >= 60) {
+            finalfare = basefare * 0.7;
+            printf("Passenger: %s - Base Fare: ₹%.2f - Senior Citizen Discount Applied", name, basefare);
+        } 
+        else {
+            printf("Passenger: %s - Base Fare: ₹%.2f", name, basefare);
+        }
+
+        if ((time >= 8 && time <= 10) || (time >= 17 && time <= 20)) {
+            finalfare = finalfare * 1.2;
+            printf("Peak Hour Surcharge Applied");
+        }
+
+        if (finalfare < 20) {
+            finalfare = 20;
+        }
+
+        printf("Final Fare: ₹%.2f\n", finalfare);
+
+        totalrevenue += finalfare;
+
+        if (finalfare > highestfare)
+            highestfare = finalfare;
+
+        if (finalfare < lowestfare)
+            lowestfare = finalfare;
+    }
+
+    printf("\n---------- Daily Summary----------\n");
+    printf("Total Passengers: %d\n", totalpassengers);
+    printf("Total Revenue Collected: ₹%.2f\n", totalrevenue);
+    printf("Highest Fare: ₹%.2f\n", highestfare);
+    printf("Lowest Fare: ₹%.2f\n", lowestfare);
+    printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+
+    return 0;
+}
